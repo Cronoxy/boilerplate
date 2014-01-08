79ef69b3e434a40623abf35196a896ae83d7e888
Marius-Constantin Postolache <marius.postolache@avira.com>
2014-01-09 00:12:42 +0100

    updates

--- studio/index.php
+++ studio/index.php
@@ -1,6 +1,43 @@
 <?php
-$browser = get_browser(null, true);
-echo '<pre>' . print_r($browser, true) . '</pre>';
 
-echo file_get_contents('index.html');
+function crawlerDetect($USER_AGENT) {
+    $crawlers = array(
+        array('Google', 'Google'),
+        array('msnbot', 'MSN'),
+        array('Rambler', 'Rambler'),
+        array('Yahoo', 'Yahoo'),
+        array('AbachoBOT', 'AbachoBOT'),
+        array('accoona', 'Accoona'),
+        array('AcoiRobot', 'AcoiRobot'),
+        array('ASPSeek', 'ASPSeek'),
+        array('CrocCrawler', 'CrocCrawler'),
+        array('Dumbot', 'Dumbot'),
+        array('FAST-WebCrawler', 'FAST-WebCrawler'),
+        array('GeonaBot', 'GeonaBot'),
+        array('Gigabot', 'Gigabot'),
+        array('Lycos', 'Lycos spider'),
+        array('MSRBOT', 'MSRBOT'),
+        array('Scooter', 'Altavista robot'),
+        array('AltaVista', 'Altavista robot'),
+        array('IDBot', 'ID-Search Bot'),
+        array('eStyle', 'eStyle Bot'),
+        array('Scrubby', 'Scrubby robot')
+    );
+
+    foreach ($crawlers as $c) {
+        if (stristr($USER_AGENT, $c[0])) {
+            return(true);
+        }
+    }
+
+    return false;
+}
+
+// example
+
+$crawler = crawlerDetect($_SERVER['HTTP_USER_AGENT']);
+
+if (!$crawler) {
+    echo file_get_contents('index.html');
+}
 ?>
